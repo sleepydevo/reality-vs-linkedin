a2d7b5ade0a5e2f693f5a27d118b21b060b844ad
sleepydevo <sleepydevo@users.noreply.github.com>
2026-07-16 19:22:35 +0530

    Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,8 @@ export const metadata: Metadata = {
   },
 };
 
+import { Analytics } from '@vercel/analytics/react';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -31,6 +33,7 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
